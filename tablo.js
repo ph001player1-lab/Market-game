78b696f03da0e1d9d2fb243a21b06bfbc4b59262
ph001player1-lab <ph001player1@gmail.com>
2026-07-26 04:54:00 +0700

    Update tablo.js

--- tablo.js
+++ tablo.js
@@ -1,5 +1,5 @@
 // ⚠️ Та же ссылка, что и в App.js. Меняется в двух местах при новом деплое.
-var EXEC_URL = 'ВСТАВЬТЕ_СЮДА_ССЫЛКУ_НА_ВАШ_ДЕПЛОЙ/exec';
+var EXEC_URL = 'https://script.google.com/macros/s/AKfycbyZ_ArwdqpVL9JSfsQKl-Hv-qt_rdwKNFia1ZfBXV12COacMXlJDDz95JiPtU2TwLQf/exec';
 
 var METRIC_LABELS = {
   profit: 'Прибыль, ฿',
