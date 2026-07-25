b905b69e8f2d295d0fe9a85b8bbc4726fb59dd60
ph001player1-lab <ph001player1@gmail.com>
2026-07-26 00:39:32 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 // ------------------------------------------------------------ НАСТРОЙКА API
 
 // ⚠️ Единственное место, которое нужно менять при новом деплое Apps Script.
-var EXEC_URL = 'ВСТАВЬТЕ_СЮДА_ССЫЛКУ_НА_ВАШ_ДЕПЛОЙ/exec';
+var EXEC_URL = 'https://script.google.com/macros/s/AKfycbxPdiLjNCsuxwmkDJHK9CPPJ8MQbPVxhMjF4aifokPBU9WntUky_Z-GgiSvgP3t9lis/exec';
 
 function apiGet(action, params) {
   var url = EXEC_URL + '?action=' + encodeURIComponent(action);
